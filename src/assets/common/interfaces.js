ca5b1da735ec2cfc5ae52fc630433e1c878f1766
151qq <15164369647@163.com>
2017-09-30 17:13:11 +0800

    login

--- src/assets/common/interfaces.js
+++ src/assets/common/interfaces.js
@@ -7,7 +7,7 @@ const platform = "/esocial/";
 export default {
     interfaces: {
         "authentication": "/esocial/authentication.json",     // 登录
-        "getTelCode": "/static/api/common/telcode.json"       // 获取手机验证码
+        "getTelCode": "/static/api/common/telcode.json",      // 获取手机验证码
         "forgetPassword": "/static/api/common/telcode.json"   // 提交新密码
     }
 }
