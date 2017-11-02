6ec3cb39c4ef6a39f7282a31ac46f314bc60de27
151qq <15164369647@163.com>
2017-11-02 20:05:53 +0800

    del

--- src/assets/common/util.js
+++ src/assets/common/util.js
@@ -6,15 +6,6 @@ import common from './common';
 import interfaces from './interfaces';
 
 export default {
-    formatTime: function (date) {
-        var year = date.getFullYear()
-        var month = date.getMonth() + 1
-        var day = date.getDate()
-        var hour = date.getHours()
-        var minute = date.getMinutes()
-        var second = date.getSeconds()
-        return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-    },
     range: function (n) {
         n = n.toString()
         return n[1] ? n : '0' + n
